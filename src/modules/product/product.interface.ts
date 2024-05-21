aa9ffadc30ec4e05dd5d28f8ef4cb25c75a723bd
coder7475 <robiulhossain7475@gmail.com>
2024-05-21 22:07:12 +0600

    feat: add missing type for inventory in IProduct interface

--- src/modules/product/product.interface.ts
+++ src/modules/product/product.interface.ts
@@ -1,7 +1,14 @@
-type Variant = {
+// type for variant object
+type TVariant = {
   type: string;
   value: string;
 };
+// type for inventory object
+type TInventory = {
+  quantity: number;
+  inStock: boolean;
+};
+
 // Interface for a product
 interface IProduct {
   name: string;
@@ -9,7 +16,8 @@ interface IProduct {
   price: number;
   category: string;
   tags: Array<string>;
-  variants: Array<Variant>;
+  variants: Array<TVariant>;
+  inventory: TInventory;
 }
 
 export default IProduct;
