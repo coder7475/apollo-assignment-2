23c626012bec77555eac2bd262bd228c9aef156d
coder7475 <robiulhossain7475@gmail.com>
2024-05-22 05:50:24 +0600

    feat: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,16 @@
 // eslint config .mjs
-import pluginJs from '@eslint/js'
-import tseslint from 'typescript-eslint'
-import tsPlugin from '@typescript-eslint/eslint-plugin'
-import tsParser from '@typescript-eslint/parser'
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
 
 export default [
   {
     files: ['src/**/*.ts', 'src/**/*.tsx'],
     languageOptions: {
-      // globals: globals.node,
       globals: {
         process: 'readonly',
       },
-      // ecmaVersion: 'latest',
       sourceType: 'module',
       parser: tsParser,
     },
@@ -23,20 +21,16 @@ export default [
     plugins: {
       '@typescript-eslint': tsPlugin,
     },
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'prettier',
-    ],
+
     rules: {
       'no-unused-vars': 'error',
       'no-unused-expressions': 'error',
       'prefer-const': 'error',
       'no-console': 'warn',
-      'no-undef': 'error',
+      'no-undef': 'warn',
     },
   },
   {
     ignores: ['node_modules', 'dist'],
   },
-]
+];
