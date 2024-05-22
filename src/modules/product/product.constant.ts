5ba4bf984503608808f8f251c2cc1055923c373a
coder7475 <robiulhossain7475@gmail.com>
2024-05-22 20:23:36 +0600

    feat: add a contants file to store projection

---
+++ src/modules/product/product.constant.ts
@@ -0,0 +1,11 @@
+// a projection for filtering field to show
+export const projection = {
+  _id: 0,
+  name: 1,
+  description: 1,
+  price: 1,
+  category: 1,
+  tags: 1,
+  variants: 1,
+  inventory: 1,
+};
--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,3 +1,4 @@
+import { projection } from './product.constant';
 import IProduct from './product.interface';
 import { Product } from './product.model';
 
@@ -10,18 +11,6 @@ const createProductInDB = async (product: IProduct) => {
 
 // a service to retrive a list of all products
 const getAllProducts = async (searchTerm: any) => {
-  // a projection for filtering field to show
-  const projection = {
-    _id: 0,
-    name: 1,
-    description: 1,
-    price: 1,
-    category: 1,
-    tags: 1,
-    variants: 1,
-    inventory: 1,
-  };
-
   if (searchTerm) {
     // search a product with search term
     const result = await Product.find(
