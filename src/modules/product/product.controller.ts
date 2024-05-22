42d7a2b9469d4b359c18128e1835bd2057ff398c
coder7475 <robiulhossain7475@gmail.com>
2024-05-22 06:30:20 +0600

    feat: create a controller to get all products

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -24,7 +24,26 @@ const createProduct = async (req: Request, res: Response) => {
 };
 
 // controller to get all products
+const getAllProducts = async (req: Request, res: Response) => {
+  try {
+    const result = await ProductServices.getAllProducts();
+
+    // console.log(result);
+    res.status(200).json({
+      success: true,
+      message: 'Products fetched successfully!',
+      data: result,
+    });
+  } catch (error) {
+    // console.log(error);
+    res.status(404).json({
+      success: false,
+      message: 'Products fetching failed!',
+    });
+  }
+};
 
 export const ControllerOfProduct = {
   createProduct,
+  getAllProducts,
 };
